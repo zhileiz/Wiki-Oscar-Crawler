4705858442e6eaa027b15c4d853974978ee68fef
zhileiz <zhileiz@seas.upenn.edu>
2017-04-10 17:26:04 -0400

    Refactor Driver

--- src/Driver.java
+++ src/Driver.java
@@ -25,6 +25,14 @@ public class Driver {
         getLinks();
     }
 
+    private void getLinks(){
+        Element div = doc.select("table").get(1);
+        Elements hrefs = div.select("a[href]");
+        for (Element h : hrefs) {
+            links.put(h.text(), h);
+        }
+    }
+
     public void loop(){
         while (true){
             System.out.println("What do you want to know?: ");
@@ -43,14 +51,6 @@ public class Driver {
         }
     }
 
-    private void getLinks(){
-        Element div = doc.select("table").get(1);
-        Elements hrefs = div.select("a[href]");
-        for (Element h : hrefs) {
-            links.put(h.text(), h);
-        }
-    }
-
     public String returnAnswer(int k){
         String answer = "";
         String url = "";
@@ -58,193 +58,201 @@ public class Driver {
         Solution sl;
         switch (k) {
             case 1:
-                url = links.get("Best Animated Feature").attr("abs:href");
-                try {
-                    sl = new Q1(url, 1);
-                    answer = sl.getSolution();
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase1();
                 break;
             case 2:
-                url = links.get("Best Original Screenplay").attr("abs:href");
-                System.out.println("Enter the Screeplay title: ");
-                String input = src.nextLine();
-                try {
-                    sl = new Q2(url, 2, input);
-                    answer = sl.getSolution();
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase2();
                 break;
             case 3:
-                System.out.print("Enter Category: ");
-                boolean hasInput = false;
-                while (true) {
-                    String in = src.nextLine();
-                    try {
-                        in = correctFormat(in);
-                        url = links.get(in).attr("abs:href");
-                        if (url == null) {
-                            System.out.println("Gibberish Input, try again: ");
-                        } else {
-                            hasInput = true;
-                            break;
-                        }
-                    } catch (Exception e){
-                        System.out.println("Gibberish Input, try again: ");
-                        continue;
-                    }
-                }
-                System.out.println("Choose Decade of: ");
-                System.out.println("1. 1960s");
-                System.out.println("2. 1970s");
-                System.out.println("3. 1980s");
-                System.out.println("4. 1990s");
-                System.out.println("5. 2000s");
-                System.out.println("6. 2010s");
-                String ip = src.nextLine();
-                try {
-                    int it = Integer.parseInt(ip);
-                    sl = new Q3(url, 3, it);
-                    answer = sl.getSolution();
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase3();
                 break;
             case 4:
-                url = links.get("Best Leading Actor").attr("abs:href");
-                System.out.println("Please enter the role");
-                String role = src.nextLine();
-                try {
-                    sl = new Q4(url, 4, role);
-                    answer = sl.getSolution();
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase4();
                 break;
             case 5:
-                url = links.get("Best Director").attr("abs:href");
-                System.out.println("Enter Number of times: ");
-                String time = src.nextLine();
-                try {
-                    int t = Integer.parseInt(time);
-                    sl = new Q5(url, 5,t);
-                    answer = sl.getSolution();
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase5();
                 break;
             case 6:
-                System.out.print("Enter Category: ");
-                boolean isInput = false;
-                while (true) {
-                    String in = src.nextLine();
-                    try {
-                        in = correctFormat(in);
-                        url = links.get(in).attr("abs:href");
-                        if (url == null) {
-                            System.out.println("Gibberish Input, try again: ");
-                        } else {
-                            isInput = true;
-                            break;
-                        }
-                    } catch (Exception e){
-                        System.out.println("Gibberish Input, try again: ");
-                        continue;
-                    }
-                }
-                try {
-                    sl = new Q6(url, 6);
-                    answer = sl.getSolution();
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase6();
                 break;
             case 7:
-                System.out.print("Enter Category: ");
-                boolean mInput = false;
-                while (true) {
-                    String in = src.nextLine();
-                    try {
-                        in = correctFormat(in);
-                        url = links.get(in).attr("abs:href");
-                        if (url == null) {
-                            System.out.println("Gibberish Input, try again: ");
-                        } else {
-                            mInput = true;
-                            break;
-                        }
-                    } catch (Exception e){
-                        System.out.println("Gibberish Input, try again: ");
-                        continue;
-                    }
-                }
-                String it = src.nextLine();
-                try {
-                    int t = Integer.parseInt(it);
-                    sl = new Q7(url, 6,t);
-                    answer = sl.getSolution();
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase7();
                 break;
             case 8:
-                System.out.print("Enter Category1: ");
-                boolean pInput = false;
-                while (true) {
-                    String in = src.nextLine();
-                    try {
-                        in = correctFormat(in);
-                        url = links.get(in).attr("abs:href");
-                        if (url == null) {
-                            System.out.println("Gibberish Input, try again: ");
-                        } else {
-                            pInput = true;
-                            break;
-                        }
-                    } catch (Exception e){
-                        System.out.println("Gibberish Input, try again: ");
-                        continue;
-                    }
-                }
-                System.out.print("Enter Category2: ");
-                boolean qInput = false;
-                while (true) {
-                    String in = src.nextLine();
-                    try {
-                        in = correctFormat(in);
-                        subUrl = links.get(in).attr("abs:href");
-                        if (subUrl == null) {
-                            System.out.println("Gibberish Input, try again: ");
-                        } else {
-                            qInput = true;
-                            break;
-                        }
-                    } catch (Exception e){
-                        System.out.println("Gibberish Input, try again: ");
-                        continue;
-                    }
-                }
-                try {
-                    sl = new Q8(url, 8, subUrl);
-                    answer = sl.getSolution();
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                answer = workOnCase8();
                 break;
             case 9:
-                url = links.get("Best Animated Feature").attr("abs:href");
-                sl = new Q9(url, 9);
-                answer = sl.getSolution();
+                answer = workOnCase9();
                 break;
             case 10:
-                url = links.get("Best Animated Feature").attr("abs:href");
-                sl = new Q10(url, 10);
-                answer = sl.getSolution();
+                answer = workOnCase10();
                 break;
         }
         return answer;
+    }
+
+
+    private String workOnCase1(){
+        String answer = "";
+        String url = links.get("Best Animated Feature").attr("abs:href");
+        try {
+            Solution sl = new Q1(url, 1);
+            answer = sl.getSolution();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    private String workOnCase2(){
+        String answer = "";
+        String url = links.get("Best Original Screenplay").attr("abs:href");
+        System.out.println("Enter the Screeplay title: ");
+        String input = src.nextLine();
+        try {
+            Solution sl = new Q2(url, 2, input);
+            answer = sl.getSolution();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    private String workOnCase3() {
+        String answer = "";
+        System.out.print("Enter Award Category: ");
+        boolean hasInput = false;
+        String url = takeInputForUrl();
+        printYears();
+        String ip = src.nextLine();
+        try {
+            int it = Integer.parseInt(ip);
+            Solution sl = new Q3(url, 3, it);
+            answer = sl.getSolution();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    private String workOnCase4() {
+        String answer = "";
+        String url = links.get("Best Leading Actor").attr("abs:href");
+        System.out.println("Please enter the role");
+        String role = src.nextLine();
+        try {
+            Solution sl = new Q4(url, 4, role);
+            answer = sl.getSolution();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+
+    private String workOnCase5(){
+        String answer = "";
+        String url = links.get("Best Director").attr("abs:href");
+        System.out.println("Enter Number of times: ");
+        String time = src.nextLine();
+        try {
+            int t = Integer.parseInt(time);
+            Solution sl = new Q5(url, 5,t);
+            answer = sl.getSolution();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    private String workOnCase6(){
+        String answer = "";
+        System.out.print("Enter Category: ");
+        boolean isInput = false;
+        String url = takeInputForUrl();
+        try {
+            Solution sl = new Q6(url, 6);
+            answer = sl.getSolution();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    private String workOnCase7(){
+        String answer = "";
+        System.out.print("Enter Category: ");
+        boolean mInput = false;
+        String url = takeInputForUrl();
+        String it = src.nextLine();
+        try {
+            int t = Integer.parseInt(it);
+            Solution sl = new Q7(url, 6,t);
+            answer = sl.getSolution();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    private String workOnCase8(){
+        String answer = "";
+        System.out.print("Enter Category1: ");
+        boolean pInput = false;
+        String url = takeInputForUrl();
+        System.out.print("Enter Category2: ");
+        boolean qInput = false;
+        String subUrl = takeInputForUrl();
+        try {
+            Solution sl = new Q8(url, 8, subUrl);
+            answer = sl.getSolution();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    private String workOnCase9(){
+        String answer = "";
+        return answer;
+    }
+
+    private String workOnCase10(){
+        String answer = "";
+        return answer;
+    }
+
+    private String takeInputForUrl(){
+        boolean pInput = false;
+        String url = "";
+        while (true) {
+            String in = src.nextLine();
+            try {
+                in = correctFormat(in);
+                url = links.get(in).attr("abs:href");
+                if (url == null) {
+                    System.out.println("Gibberish Input, try again: ");
+                } else {
+                    pInput = true;
+                    break;
+                }
+            } catch (Exception e){
+                System.out.println("Gibberish Input, try again: ");
+                continue;
+            }
+        }
+        return url;
+    }
+
+    private String correctFormat(String str){
+        str = str.toLowerCase();
+        String st[] = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String s : st){
+            sb.append(s.substring(0,1).toUpperCase() + s.substring(1));
+            sb.append(" ");
+        }
+        return sb.toString().substring(0,sb.length()-1);
     }
 
     private void printQuestions(){
@@ -262,15 +270,14 @@ public class Driver {
         System.out.println("!! Or type 'quit' or 'exit' to exit !!");
     }
 
-    private String correctFormat(String str){
-        str = str.toLowerCase();
-        String st[] = str.split(" ");
-        StringBuilder sb = new StringBuilder();
-        for (String s : st){
-            sb.append(s.substring(0,1).toUpperCase() + s.substring(1));
-            sb.append(" ");
-        }
-        return sb.toString().substring(0,sb.length()-1);
+    private void printYears(){
+        System.out.println("Choose Decade of: ");
+        System.out.println("1. 1960s");
+        System.out.println("2. 1970s");
+        System.out.println("3. 1980s");
+        System.out.println("4. 1990s");
+        System.out.println("5. 2000s");
+        System.out.println("6. 2010s");
     }
 
 }
